aa9ae44deacf1349b0dd7e963e1ff20df587e5d9
dclarino <davidbclarino@gmail.com>
2014-12-07 15:00:11 -0800

    added constraints to mem test

--- tb/transactions/memory_transaction.svh
+++ tb/transactions/memory_transaction.svh
@@ -1,13 +1,26 @@
 class memory_transaction extends command_transaction;
    `uvm_object_utils(memory_transaction)
-
-	 constraint data {
+   
+   constraint data {
+      solve op before op_pf sv A;
+		    
+      if ((op == 8'h00) || (op == 8'h01)) {
+          op_pf == 1;
+          sv == 0;
+      } else if ((op == 8'h06) || (op == 8'h07)) {
+          op_pf == 1;
+      } else if ((op == 8'h08) || (op == 8'h09)) {
+         op_pf == 1;
+         sv == 0;
+      }         
+   }
+/*	 constraint data {
 	 		  A > 32'h01;
 			  A < 32'hEEEEFFFF;
 			op_pf == 1'b1;
 			sv == 1'b0;
 	    op < 8'h0b;
-		}
+		} */
 //   constraint data { A dist {8'h00:=1, [8'h01 : 8'hFE]:=1, 8'hFF:=1};
 //                     B dist {8'h00:=1, [8'h01 : 8'hFE]:=1, 8'hFF:=1};} 
    
